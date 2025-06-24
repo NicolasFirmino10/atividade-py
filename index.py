8ed85786a053e2b3a6f571c55c5e115d76a6f56d
NICOLASFIRMINO10 <nicolasneuma10@gmail.com>
2025-06-24 20:26:38 -0300

    first commit

---
+++ index.py
@@ -0,0 +1,39 @@
+# Programa que exibe uma saudação
+print("Olá, mundo!")
+
+
+
+# Soma de dois números
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+
+print("A soma é:", soma)
+
+
+
+# Cálculo do volume de uma caixa retangular
+comprimento = 12  # cm
+largura = 14      # cm
+altura = 20       # cm
+
+volume = comprimento * largura * altura
+
+print("O volume da caixa é:", volume, "cm³")
+
+
+
+# Cálculo do preço total de uma compra
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+
+print("Produto:", nome_produto)
+print("Preço unitário: R$", preco_unitario)
+print("Quantidade:", quantidade)
+print("Preço total: R$", round(preco_total, 2))
+
+
+
